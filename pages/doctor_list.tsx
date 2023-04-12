43652b3a2b19f63f55eae69d46b1a5f9ad2e296c
sthitiacharya <sthitiacharya@gmail.com>
2023-04-12 10:39:48 +0800

    integrated backend with frontend for doctor list

--- pages/doctor_list.tsx
+++ pages/doctor_list.tsx
@@ -1,35 +1,59 @@
 import Layout from "@/components/layout";
 import { motion } from "framer-motion";
 import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
+import {useEffect, useState} from "react";
+import apiClient from "@/pages/utils/apiClient";
+import {useRouter} from "next/router";
+import Cookies from "js-cookie";
 
+/*
 const doctors = [
   {
     id: 1,
     name: "Dr. John Smith",
     specialty: "Cardiologist",
-    imageUrl: "https://via.placeholder.com/150",
   },
   {
     id: 2,
     name: "Dr. Jane Doe",
     specialty: "Dermatologist",
-    imageUrl: "https://via.placeholder.com/150",
   },
   {
     id: 3,
     name: "Dr. Alice Johnson",
     specialty: "Neurologist",
-    imageUrl: "https://via.placeholder.com/150",
   },
   {
     id: 4,
     name: "Dr. Bob Brown",
     specialty: "Orthopedic Surgeon",
-    imageUrl: "https://via.placeholder.com/150",
   },
 ];
+*/
 
 export default function Doctors() {
+
+  const [doctors, setDoctorsData] = useState([{id: "", name : "", hospitalName: ""}]);
+
+  const router = useRouter();
+
+    useEffect(() => {
+      let userToken = Cookies.get("userToken");
+      if (userToken == null) {
+          router.push('/login')
+      }
+
+      apiClient
+          .get('/list/doctor', {headers: {Authorization: `Bearer ${userToken}`}})
+          .then((response) => {
+              console.log(response.data)
+              setDoctorsData(response.data)
+          })
+          .catch(err => {
+            alert(err);
+          })
+  }, [])
+
   return (
     <Layout>
       <motion.div
@@ -63,14 +87,9 @@ export default function Doctors() {
               key={doctor.id}
               className="flex items-center space-x-4 p-4 bg-white border border-gray-300 rounded-md"
             >
-              <img
-                src={doctor.imageUrl}
-                alt={doctor.name}
-                className="w-16 h-16 rounded-full"
-              />
               <div>
                 <h2 className="text-xl font-bold">{doctor.name}</h2>
-                <p className="text-gray-600">{doctor.specialty}</p>
+                <p className="text-gray-600">{doctor.hospitalName}</p>
               </div>
             </div>
           ))}
