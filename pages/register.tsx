adc612f61303f00c12a9a526849723dc59ce3077
Changrui <78465180+Ch40gRv1-Mu@users.noreply.github.com>
2023-04-12 16:26:16 +0800

    add registration for both patient and doctor

--- pages/register.tsx
+++ pages/register.tsx
@@ -2,11 +2,11 @@ import Layout from "@/components/layout";
 import { motion } from "framer-motion";
 import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
 import Link from "next/link";
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
-
+import { useEffect } from "react";
 import apiClient from "@/pages/utils/apiClient";
-import { addDoctor } from "web3_api/";
+import { addPatient, addDoctor } from "web3_api/";
 
 export default function Register() {
   const router = useRouter();
@@ -20,35 +20,84 @@ export default function Register() {
   const [weight, setWeight] = useState();
   const [history, setHistory] = useState("");
   const [allergies, setAllergies] = useState("");
+  const [hospitalName, setHospitalName] = useState("");
+  const [userType, setUserType] = useState(null);
+  useEffect(() => {
+    setUserType("patient");
+  }, []);
+
   function passwordsMatch(password: string, confirmedPassword: string): boolean {
     return password === confirmedPassword;
   }
+
   const handleReg = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    try{
+    try {
       if (!passwordsMatch(password, confirmedPassword)) {
         alert("Passwords do not match");
         return;
       }
-      const tx = await addPatient(walletAddress);
-      console.log("Transaction hash:", tx.hash);
-      apiClient.post('/signup/patient', {
-        name, email, password, wallet_address: walletAddress,
-        profile: {dob, height, weight, history, allergies, name, patient_wallet: walletAddress }
-      }).then(response => {
-        alert("Registration successful, please login");
-        router.push("/login");
-      }).catch(err => {
-        alert(err);
-      })
-    } catch(err) {
+
+      if (userType === "patient") {
+        const tx = await addPatient(walletAddress);
+        console.log("Transaction hash:", tx.hash);
+        apiClient
+          .post("/signup/patient", {
+            name,
+            email,
+            password,
+            wallet_address: walletAddress,
+            profile: {
+              dob,
+              height,
+              weight,
+              history,
+              allergies,
+              name,
+              patient_wallet: walletAddress,
+            },
+          })
+          .then((response) => {
+            alert("Registration successful, please login");
+            router.push("/login");
+          })
+          .catch((err) => {
+            alert(err);
+          });
+      } else if (userType === "doctor") {
+        const tx = await addDoctor(walletAddress);
+        console.log("Transaction hash:", tx.hash);
+        apiClient
+          .post("/signup/doctor", {
+            name,
+            email,
+            password,
+            wallet_address: walletAddress,
+            profile: {
+              hospital_name: hospitalName,
+              name,
+              doctor_wallet: walletAddress,
+            },
+          })
+          .then((response) => {
+            alert("Registration successful, please login");
+            router.push("/login");
+          })
+          .catch((err) => {
+            alert(err);
+          });
+      }
+    } catch (err) {
       alert(err);
       console.error("Error adding prescription:", err);
-    }   
-  }
-  
+    }
+  };
+
+
   return (
     <Layout>
+
+
       <motion.div
         className="max-w-xl mx-auto px-5 xl:px-0"
         initial="hidden"
@@ -76,6 +125,24 @@ export default function Register() {
           variants={FADE_DOWN_ANIMATION_VARIANTS}
          onSubmit={handleReg}
         >
+          {/* Add a switch to choose user type */}
+        <div className="flex justify-center">
+          <div className="w-1/2">
+            <label htmlFor="userType" className="block text-gray-600">
+              I am a
+            </label>
+            <select
+              id="userType"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+              onChange={(e) => setUserType(e.target.value)}
+              required
+              >
+              <option value="patient">Patient</option>
+              <option value="doctor">Doctor</option>
+              </select>
+          </div>
+        </div>
+
           <div className="flex space-x-4 center">
             <div className="w-1/2">
               <label htmlFor="name" className="block text-gray-600">
@@ -176,28 +243,46 @@ export default function Register() {
               required
             />
           </div>
-          <div>
-            <label htmlFor="text" className="block text-gray-600">
-              Medical History
-            </label>
-            <textarea
-              id="history"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              onChange={(e) => setHistory(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="text" className="block text-gray-600">
-              Allergies
-            </label>
-            <textarea
-              id="allergies"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              onChange={(e) => setAllergies(e.target.value)}
-              required
-            />
-          </div>
+          {userType === "doctor" && (
+            <div>
+              <label htmlFor="hospitalName" className="block text-gray-600">
+                Hospital Name
+              </label>
+              <input
+                type="text"
+                id="hospitalName"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                onChange={(e) => setHospitalName(e.target.value)}
+                required
+              />
+            </div>
+          )}
+           {userType === "patient" && (
+            <>
+              <div>
+                <label htmlFor="text" className="block text-gray-600">
+                  Medical History
+                </label>
+                <textarea
+                  id="history"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                  onChange={(e) => setHistory(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="text" className="block text-gray-600">
+                  Allergies
+                </label>
+                <textarea
+                  id="allergies"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                  onChange={(e) => setAllergies(e.target.value)}
+                  required
+                />
+              </div>
+            </>
+          )}
           <div>
             <button
               type="submit"
@@ -206,8 +291,8 @@ export default function Register() {
               Register
             </button>
           </div>
+          
         </motion.form>
-
         <motion.div
           className="mt-4 text-center text-gray-500"
           variants={FADE_DOWN_ANIMATION_VARIANTS}
